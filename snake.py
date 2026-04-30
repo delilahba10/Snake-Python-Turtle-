d5715caa4ce0181e4b9b6f94a6ef6356143694c0
Your Name <delilahbatkinson@gmail.com>
2026-04-30 16:13:57 -0500

    died message and working apple

--- snake.py
+++ snake.py
@@ -22,7 +22,7 @@ class Head(Turtle):
     super().__init__()
     self.ht
     self.speed(25)
-    self.color("black")
+    self.color("blue")
     self.penup()
     self.goto(x, y)
     self.setheading(90)
@@ -48,14 +48,12 @@ class Head(Turtle):
 
   def move(self):
     self.forward(4)
-    if self.xcor() > 230 or self.xcor() < -230:
-      self.setheading(180 - self.heading())
-    if self.ycor() > 230 or self.ycor() < -230:
-      self.setheading(-self.heading())
+    if self.xcor() > 230 or self.xcor() < -225 or self.ycor() > 230 or self.ycor() < -228:
+      self.die()
     
   def die(self):
-    pass
-
+    self.ht()
+    self.alive = False
 
 class Segment(Turtle):
   def __init__(self, other):
@@ -68,7 +66,8 @@ class Segment(Turtle):
 class Apple(Turtle):
   def __init__(self):
     super().__init__()
-    self.hideturtle()
+    self.ht()
+    self.speed(0)
     self.color('red')
     self.shape('circle')
     self.penup()
@@ -95,5 +94,14 @@ while player.alive:
   player.move()
   if player.distance(apple) < 20:
     apple.relocate()
+
+apple.ht()
+yertle = Turtle()
+yertle.ht()
+yertle.penup()
+yertle.goto(-125, -25)
+yertle.color("red")
+yertle.speed(0)
+yertle.write("You died", font=("Arial", 50, "normal"))
 
 screen.exitonclick()
